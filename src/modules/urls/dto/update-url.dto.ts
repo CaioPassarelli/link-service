6ad9665922b5edf9161e0b5a41e638f49ff0e434
Caio Passarelli <caiopassarelli22@gmail.com>
2026-02-08 17:54:20 -0300

    fix(swagger): add example in update url endpoint

--- src/modules/urls/dto/update-url.dto.ts
+++ src/modules/urls/dto/update-url.dto.ts
@@ -1,8 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUrlDto } from './create-url.dto';
 import { IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUrlDto extends PartialType(CreateUrlDto) {
+    @ApiProperty({
+        description: 'Novo valor de URL.',
+        example: 'https://www.google.com/'
+    })
     @IsOptional()
     originalUrl?: string
 }
