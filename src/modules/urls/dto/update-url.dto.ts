5d16c77bba6ea8afd47554cc18dbbc0729a11c74
Caio Passarelli <caiopassarelli22@gmail.com>
2026-02-08 16:11:55 -0300

    feat(crud-url): create crud functions to url service

--- src/modules/urls/dto/update-url.dto.ts
+++ src/modules/urls/dto/update-url.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUrlDto } from './create-url.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateUrlDto extends PartialType(CreateUrlDto) {}
+export class UpdateUrlDto extends PartialType(CreateUrlDto) {
+    @IsOptional()
+    originalUrl?: string
+}
